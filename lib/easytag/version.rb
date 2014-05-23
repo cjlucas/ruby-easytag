181e0d5f63857b930737d770133f5d3ff2017e91
Chris Lucas <chris@chrisjlucas.com>
2014-05-22 18:07:17 -0700

    bump to 0.5.0

--- lib/easytag/version.rb
+++ lib/easytag/version.rb
@@ -1,8 +1,8 @@
 module EasyTag
   class Version
     MAJOR = 0
-    MINOR = 4
-    TINY  = 3
+    MINOR = 5
+    TINY  = 0
   end
  
   VERSION = [Version::MAJOR, Version::MINOR, Version::TINY].join('.')
