5c1ab33ce231cf6fe2bb636aa06b5ada7dda72d4
Chris Lucas <chris@chrisjlucas.com>
2014-05-22 20:29:37 -0700

    [attributes] moved audio_prop_reader into base class

--- lib/easytag/attributes/base.rb
+++ lib/easytag/attributes/base.rb
@@ -1,5 +1,14 @@
 module EasyTag
   module BaseAttributeAccessors
+
+    def audio_prop_reader(attr_name, prop_name = nil, **opts)
+      prop_name = attr_name if prop_name.nil?
+      define_method(attr_name) do
+        v = self.class.read_audio_property(taglib, prop_name)
+        self.class.post_process(v, opts)
+      end
+    end
+
     def cast(data, opts)
       case opts[:returns]
       when :int
@@ -18,6 +27,10 @@ module EasyTag
     def post_process(data, opts)
       data = Utilities.get_int_pair(data) if opts[:returns] == :int_pair
       cast(data, opts)
+      end
+
+    def read_audio_property(taglib, key)
+      taglib.audio_properties.send(key)
     end
   end
 end
