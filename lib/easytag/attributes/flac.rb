045b23bb59e6d25344f47fb9303790d4a5356443
Chris Lucas <chris@chrisjlucas.com>
2014-05-25 14:20:02 -0700

    [flac] moved album_art_reader into accessors module

--- lib/easytag/attributes/flac.rb
+++ lib/easytag/attributes/flac.rb
@@ -9,5 +9,17 @@ module EasyTag
         self.class.post_process(v, **opts)
       end
     end
+
+    def album_art_reader(attr_name, **opts)
+      define_method(attr_name) do
+        taglib.picture_list.collect do |pic|
+          EasyTag::Image.new(pic.data).tap do |img|
+            img.mime_type = pic.mime_type
+            img.type      = pic.type
+          end
+        end
+      end
+    end
+
   end
 end
